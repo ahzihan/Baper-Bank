fcd8744e34b9e6a8f40fa5a41b767b948d825356
Md Akbar Hossain <akbarhossain160@gmail.com>
2022-02-23 13:50:34 +0600

    first commit

--- js/banking.js
+++ js/banking.js
@@ -1,37 +1,64 @@
+
+//Deposit Section
 document.getElementById( 'deposit-btn' ).addEventListener( 'click', function () {
-    const depositInputText = document.getElementById( 'deposit-input' );
-    const depositInputValue = parseFloat( depositInputText.value );
-    // console.log( depositInputValue );
-    const depositeBalanceText = document.getElementById( 'deposit-balance' );
-    const depositBalance = parseFloat( depositeBalanceText.innerText );
-    // console.log( depositBalance );
-    const depositAmount = depositInputValue + depositBalance;
-    depositeBalanceText.innerText = depositAmount;
-    //Update Total
-    const totalBalanceText = document.getElementById( 'total-balance' );
-    const totalBalanceDeposit = parseFloat( totalBalanceText.innerText );
-    totalBalanceText.innerText = totalBalanceDeposit + depositInputValue;
+    const depositInputField = document.getElementById( 'deposit-input' );
+    const depositInputValue = parseFloat( depositInputField.value );
+    if ( depositInputValue > 0 ) {
+        const depositBalanceText = document.getElementById( 'deposit-balance' );
+        const depositBalanceValue = parseFloat( depositBalanceText.innerText );
+        depositBalanceText.innerText = depositBalanceValue + depositInputValue;
+
+        //update Total
+        const balanceTotalField = document.getElementById( 'total-balance' );
+        const totalBalance = parseFloat( balanceTotalField.innerText );
+        balanceTotalField.innerText = totalBalance + depositInputValue;
+    }
+    
+
     //clear input
-    depositInputText.value = '';
+    depositInputField.value = '';
+
 } );
 
-//Withdraw
-document.getElementById( 'withdraw-btn' ).addEventListener( 'click', function () { 
-    const withdrawInputText = document.getElementById( 'withdraw-input' );
-    const withdrawInputValue = parseFloat( withdrawInputText.value );
-
-    const withdrawBalanceText = document.getElementById( 'withdraw-balance' );
-    const withdrawBalance = parseFloat( withdrawBalanceText.innerText );
-
-    const withdrawAmount = withdrawInputValue + withdrawBalance;
-    withdrawBalanceText.innerText = withdrawAmount;
-
-    //Update Total
-    const totalBalanceText = document.getElementById( 'total-balance' );
-    const totalBalanceWithdraw = parseFloat( totalBalanceText.innerText );
-    totalBalanceText.innerText = totalBalanceWithdraw - withdrawInputValue;
-    //clear input
-    withdrawInputText.value = '';
-
+//Withdraw Section 
+document.getElementById( 'withdraw-btn' ).addEventListener( 'click', function () {
+    const withdrawInputField = document.getElementById( 'withdraw-input' );
+    const withdrawInputValue = parseFloat( withdrawInputField.value );
+    if ( withdrawInputValue > 0 ) {
+        const withdrawBalanceText = document.getElementById( 'withdraw-balance' );
+        const withdrawBalanceValue = parseFloat( withdrawBalanceText.innerText );
+        withdrawBalanceText.innerText = withdrawBalanceValue + withdrawInputValue;
+        
+        //update Total
+        const balanceTotalField = document.getElementById( 'total-balance' );
+        const totalBalance = parseFloat( balanceTotalField.innerText );
+        balanceTotalField.innerText = totalBalance - withdrawInputValue;
+    }
+    //Clear input
+    withdrawInputField.value = '';
 
 } );
+
+
+// function getInputField(inputId) {
+//     const inputField = document.getElementById( inputId );
+//     const inputValue = parseFloat( inputField.value );
+//     inputField.value = '';
+//     return inputValue;
+// }
+// function getTextField( textFieldId,amount ) {
+//     const balanceText = document.getElementById( textFieldId, );
+//     const balanceValue = parseFloat( balanceText.innerText );
+//     balanceText.innerText = balanceValue + amount;
+// }
+// function updateTotral(amount,isAdd) {
+//     const balanceTotalField = document.getElementById( 'total-balance' );
+//     const totalBalance = parseFloat( balanceTotalField.innerText );
+    
+//     if ( isAdd==true ) {
+//         balanceTotalField.innerText = totalBalance + amount;
+//     }else {
+//         balanceTotalField.innerText = totalBalance - amount;
+//     }
+    
+// }
--- js/login.js
+++ js/login.js
@@ -1,12 +1,12 @@
 document.getElementById( 'login-btn' ).addEventListener( 'click', function () {
     const userEmailField = document.getElementById( 'user-email' );
     const userEmail = userEmailField.value;
-    const userPasswordField = document.getElementById( 'user-password' );
-    const userPassword = userPasswordField.value;
+    const userPasswordfield = document.getElementById( 'user-password' );
+    const userPassword = userPasswordfield.value;
     if ( userEmail == 'ahzihan@gmail.com' && userPassword == 'ahzihan' ) {
         window.location.href = 'baper-bank.html';
     }
     else {
-        alert( 'Please, Enter your valid email or password!' );
+        alert( 'Please, Enter valid email or password!' );
     }
-} );
+})
